36e29eb2a441ce4c51b4eed461eec795deb14695
Cedric Mensah <cedric@Cedrics-MacBook-Pro-2.local>
2019-01-26 09:48:08 +1100

    Improved like algorithm

--- contentScript.js
+++ contentScript.js
@@ -1,44 +1,26 @@
-console.log("Hello World");
-alert("content Script running");
+alert("Liking all Instagram Posts in your feed...");
 
-var i = 0;
+var counter = 0;
 
- setInterval(openFeed, 60000);
-   
+ setInterval(likePosts, 60000);
 
-
-
-
-
-// 
-
-
-
-function openFeed() {
+function likePosts() {
+    
+        window.scrollBy(0, 3000);
     
         var likes = document.getElementsByClassName("dCJp8 afkep coreSpriteHeartOpen _0mzm-");
-
                   
         for(var item of likes) {
             if(item.firstChild.className == "glyphsSpriteHeart__outline__24__grey_9 u-__7") {
-                item.click()
-                console.log("liked")
+                item.click();
+                console.log("liked");
 
             } 
-
-        }      
+        }           
         
-        
-            window.scrollBy(0, 3000);
-            console.log("Iteration: " + i)
-            i++
+        console.log("Iteration: " + counter);
+        counter++;
 
-   
-   
 }
-
-function changePage() {
-    window.location.href = "https://www.google.de" 
- }
 
 
