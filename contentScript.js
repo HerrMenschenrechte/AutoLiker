fe1a9220a8b6857b21776fe322d6ad8ac2c6c68f
Cedric Mensah <cedric@Cedrics-MacBook-Pro-2.local>
2019-02-04 08:31:10 +1100

    Improved liking algorithm

--- contentScript.js
+++ contentScript.js
@@ -1,35 +1,29 @@
 alert("Liking all Instagram Posts in your feed...");
 
 var counter = 0;
+var likes = null;
 
 console.log("Script is running...")
 
 setInterval(likePosts, 60000);
 
 // create function that checks if posts are already liked to speed up the liking process
-
-
  
 function likePosts() {
-    
-        window.scrollBy(0, 3000);
-    
         var likes = document.getElementsByClassName("dCJp8 afkep coreSpriteHeartOpen _0mzm-");
+        console.log(likes)
                   
         for(var item of likes) {
             if(item.firstChild.className == "glyphsSpriteHeart__outline__24__grey_9 u-__7") {
                 item.click();
-                console.log("liked");
-
-            } else {
-                window.scrollBy(0, 3000);
-
+                console.log(JSON.stringify(item.firstChild.className) + " liked")
             }
-        }           
+        }     
+        
+        window.scrollBy(0, 2000);
         
         console.log("Iteration: " + counter);
         counter++;
 
 }
-
 
